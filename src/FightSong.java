614926c34c302f5686ef83c70722f7d7f25828d6
EliFJ <jonesel@ssfs.org>
2021-09-29 12:56:57 -0600

    Called methods to print a song

--- src/FightSong.java
+++ src/FightSong.java
@@ -1,19 +1,45 @@
 public class FightSong {
 
+
     /*
     This method is static, which means you don't need to make a new instance of FightSong in order to use it.
     It is also void, which means it does not return any information. It should be marked private so that it can
     only be used in this class. Since I need to access it for testing purposes, it must be public.
      */
     public static void goTeam() {
-        // Add any print statements you think should go in this method.
-
+        System.out.println("Go, team, go!");
+        System.out.println("You can do it.");
     }
 
-    // Create any additional methods you need in this space in order to fulfill the requirements of the program
+    public static void theBest() {
+        System.out.println("You're the best,");
+        System.out.println("In the West.");
+    }
+    public static void space()
+    {
+        System.out.println();
+    }
 
-    public static void main(String[] args) {
-        // No print statements should be in here, only method calls.
+    public static void main(String[] args)
+    {
+        goTeam();
+        space();
+        goTeam();
+        theBest();
+        goTeam();
+        space();
+        goTeam();
+        theBest();
+        goTeam();
+        space();
+        goTeam();
+
 
     }
 }
+
+
+
+
+
+
