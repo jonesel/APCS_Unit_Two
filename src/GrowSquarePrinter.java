94d27dc543909f1dba9b00788b8dcf8d631ea80c
EliFJ <jonesel@ssfs.org>
2021-10-05 14:59:45 -0600

    Translated and grew a box while keeping it in the same corner

--- src/GrowSquarePrinter.java
+++ src/GrowSquarePrinter.java
@@ -2,7 +2,15 @@ import java.awt.*;
 
 public class GrowSquarePrinter {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+        Rectangle square = new Rectangle(100, 100, 50 ,50);
+        System.out.println(square);
+        square.grow(25,25);
+        square.translate(25,25);
+        System.out.println(square);
+
+
 
         // Create a Rectangle object with a starting x, y of (100, 100) and a width and height of 50.
 
