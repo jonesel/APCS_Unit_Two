94589fee7ae60daf18fa866c55949fde9edb9a28
EliFJ <jonesel@ssfs.org>
2021-10-07 09:08:54 -0600

    Having issue with errors

--- src/RectangleFun.java
+++ src/RectangleFun.java
@@ -7,6 +7,54 @@ public class RectangleFun {
         // Create a scanner object to be able to get input from the user
         Scanner scan = new Scanner(System.in);
 
+        public double getX();
+    {
+            System.out.println("Please enter the x-coordinate of your rectangle: ");
+            scan.nextInt();
+    }
+
+
+
+        public double getY();
+    {
+        System.out.println("Please enter the y-coordinate of your rectangle: ");
+        scan.nextInt();
+    }
+
+        public double getWidth();
+    {
+            System.out.println("Please enter the width of your rectangle: ");
+            scan.nextInt();
+
+    }
+
+        public double getHeight();
+    {
+        System.out.println("Please enter the height of your rectangle: ");
+        scan.nextDouble();
+
+        return 3.5;
+    }
+
+
+
+        public void setLocation(int x, int y);
+        {
+
+        }
+
+
+        public Rectangle(int x,int y,int width,int height);
+        {
+            System.out.println("X: "+getX);
+            System.out.println("Y: "+getY);
+            System.out.println("Width: "+getWidth);
+            System.out.println("Height: "+getHeight);
+        }
+public void perimeter();
+    {
+        System.out.println("Perimeter: "+2*(getHeight+getWidth));
+    }
         // Get input for x, y, width, and height. Create four separate variables for these.
 
 
@@ -18,4 +66,5 @@ public class RectangleFun {
         // Use getX(), getY(), and setLocation() to move the Rectangle. Print the new location.
 
     }
+
 }
